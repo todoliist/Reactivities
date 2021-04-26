f3dfa270a4e67b2bee9030010da10013d03ecbbe
Tim Su <ssu851009@hotmail.com>
2021-04-26 17:40:11 -0600

    Use Mediator to handle list activities/details req

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,28 +1,24 @@
-using Persistence;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Domain;
-using Microsoft.EntityFrameworkCore;
 using System;
+using Application.Activities;
+
 namespace API.Controllers
 {
-    public class ActivitiesController:BaseApiController
+    public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await _context.Activities.ToListAsync();
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id){
-            return await _context.Activities.FindAsync(id);
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
         }
     }
 }
